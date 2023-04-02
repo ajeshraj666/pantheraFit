ae2f361b4ff53e2fc7a82fbf65b5a7574890d5ba
Ajeesh Rajan <64639567+ajeshraj666@users.noreply.github.com>
2023-04-03 01:09:22 +0530

    hi
    hi

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+"use strict";
+let mobBtn = document.querySelector('.mob-btn')
+let menu = document.querySelector('.menu-wrapper')
+  mobBtn.addEventListener('click', function(){
+    menu.classList.toggle('show-menu');
+  })
+
+
+
+
+  
+
+  //Testimonial Slider
+  $('.testimonialSlider').slick({
+    slidesToShow:1,
+    slideToScroll: 1,
+    autoplay:true,
+    dots: false,
+    arrows: false,
+    responsive: [
+     {
+       breakpoint: 1200,
+       settings: {
+         slidesToShow:1,
+         slidesToScroll: 1,
+         
+       }
+     },
+     {
+       breakpoint: 768,
+       settings: {
+         slidesToShow: 1,
+         slidesToScroll: 1,
+       }
+     },
+     {
+       breakpoint: 767,
+       settings: {
+         slidesToShow: 1,
+         slidesToScroll: 1,
+       }
+     }
+     ],
+
+
+  });
+
+
+
